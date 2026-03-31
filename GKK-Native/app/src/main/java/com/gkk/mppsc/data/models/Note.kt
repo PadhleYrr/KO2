e8500093a176479bc3a28e9f402c99d2ffcd0b70
PadhleYrr <moliage490@gmail.com>
2026-03-31 14:06:10 +0530

    Delete GKK-Native directory

--- GKK-Native/app/src/main/java/com/gkk/mppsc/data/models/Note.kt
+++
@@ -1,9 +0,0 @@
-package com.gkk.mppsc.data.models
-
-import com.google.gson.annotations.SerializedName
-
-data class Note(
-    @SerializedName("id")      val id:      String = "",
-    @SerializedName("name")    val name:    String = "",
-    @SerializedName("content") val content: String = ""   // HTML string rendered in a WebView
-)
--- GKK-Native/app/src/main/java/com/gkk/mppsc/ui/screens/Screens.kt
+++
@@ -1,4 +0,0 @@
-package com.gkk.mppsc.ui.screens
-// All screens implemented in individual files:
-// DashboardScreen.kt, TestScreens.kt, NotesScreen.kt,
-// FlashcardsScreen.kt, RemainingScreens.kt
