d489bcb46fcd805bc055815d4b6a31a4bd5421c1
PadhleYrr <moliage490@gmail.com>
2026-03-31 15:32:53 +0530

    Add files via upload

--- app/src/main/java/com/gkk/mppsc/GKKApp.kt
+++ app/src/main/java/com/gkk/mppsc/GKKApp.kt
@@ -1,11 +1,12 @@
 package com.gkk.mppsc
 
 import android.app.Application
-import com.google.firebase.FirebaseApp
 
 class GKKApp : Application() {
+    
     override fun onCreate() {
         super.onCreate()
-        FirebaseApp.initializeApp(this)
+        // Initialize your app components here
+        // For example: Firebase, Analytics, etc.
     }
 }
