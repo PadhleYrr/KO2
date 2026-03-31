80f65b63e0b3c484f06a4dbeab2e7d4e4046bf25
PadhleYrr <moliage490@gmail.com>
2026-03-31 13:58:39 +0530

    unzipgit

---
+++ GKK-Native/app/src/main/java/com/gkk/mppsc/data/models/Syllabus.kt
@@ -0,0 +1,16 @@
+package com.gkk.mppsc.data.models
+
+import com.google.gson.annotations.SerializedName
+
+data class SyllabusPaper(
+    @SerializedName("paper")    val paper:    String           = "",
+    @SerializedName("icon")     val icon:     String           = "",
+    @SerializedName("color")    val color:    String           = "#1A237E",
+    @SerializedName("subjects") val subjects: List<SyllabusSubject> = emptyList()
+)
+
+data class SyllabusSubject(
+    @SerializedName("name")   val name:   String       = "",
+    @SerializedName("icon")   val icon:   String       = "",
+    @SerializedName("topics") val topics: List<String> = emptyList()
+)
