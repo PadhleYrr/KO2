80c1cf6bfa8788af34a5b604259370278494a4d8
PadhleYrr <moliage490@gmail.com>
2026-04-01 11:43:16 +0530

    unzipgit

---
+++ app/src/main/java/com/padhleyrr/mppsc/GKKApp.kt
@@ -0,0 +1,12 @@
+package com.padhleyrr.mppsc
+
+import android.app.Application
+import com.google.firebase.FirebaseApp
+
+class GKKApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
